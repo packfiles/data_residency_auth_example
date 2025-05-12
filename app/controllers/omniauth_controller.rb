995d280ac08866a2a3a2c7fb5a6552b46f83d0a7
Chase Southard <chase.southard@gmail.com>
2025-05-12 15:15:58 -0400

    Updates OmniAuth controller to set session data

--- app/controllers/omniauth_controller.rb
+++ app/controllers/omniauth_controller.rb
@@ -1,22 +1,25 @@
 class OmniauthController < Devise::OmniauthCallbacksController
+  skip_before_action :verify_authenticity_token, only: [ :github ]
   def github
+    Rails.logger.info("Omniauth github: #{request.env['omniauth.auth']}")
+
     auth = request.env["omniauth.auth"]
     @user = User.from_omniauth(auth)
-    if @user.present?
-      .slug = auth.info.nickname
-      .name = auth.info.name
-      .image_remote_url = auth.info.image
-      .title = auth.extra.raw_info.bio
-      .location = auth.extra.raw_info.location
-      .homepage_uri = auth.extra.raw_info.blog if auth.extra.raw_info.blog.present?
-      .github_uri = "https://github.com/#{auth.info.nickname}"
-      .save if @user.changed?
+    if @user.persisted?
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", kind: "Github"
       sign_in(resource_name, @user)
-      redirect_to root_url, event: :authentication
+      session[:user_id] = @user.id
+      session[:provider] = auth.provider
+      session[:uid] = auth.uid
+      redirect_to profile_user_path(@user), event: :authentication
     else
-      redirect_to root_url, alert: "Unable to sign in via Github"
+      redirect_to root_url, alert: "Unable to sign in via Github."
     end
   end
 
+  def failure
+    Rails.logger.error("Omniauth failure: #{request.env['omniauth.error']}")
+    Rails.logger.error("Omniauth failure: #{request.env['omniauth.error.strategy']}")
+    redirect_to root_url, alert: "Failed to sign in via Github! 😰"
+  end
 end
