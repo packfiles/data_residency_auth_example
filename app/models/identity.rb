f6c555ca816363a4c903b1685ba47b0c3ec5ead0
Chase Southard <chase.southard@gmail.com>
2025-05-13 15:53:11 -0400

    Adds identity class methods for initalizations

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -1,4 +1,12 @@
 class Identity < ApplicationRecord
   belongs_to :user, counter_cache: true
   has_one :identity_profile, dependent: :destroy
+
+  def self.find_with_omniauth(auth)
+    find_by(uid: auth["uid"], provider: auth["provider"])
+  end
+
+  def self.create_with_omniauth(auth)
+    create(uid: auth["uid"], provider: auth["provider"])
+  end
 end
