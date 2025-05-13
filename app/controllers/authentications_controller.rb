1d8ddc202538d14892bebdc9b4fe374b26e415c3
Chase Southard <chase.southard@gmail.com>
2025-05-13 15:45:41 -0400

    Adds an authentications controller for sessions

---
+++ app/controllers/authentications_controller.rb
@@ -0,0 +1,53 @@
+class AuthenticationsController < Devise::SessionsController
+  prepend_before_action { request.env["devise.skip_timeout"] = true }
+  skip_before_action :verify_authenticity_token
+
+  def new
+    Rails.logger.info("!!!!!! AuthenticationsController#new !!!!!!")
+
+    super
+  end
+
+  def create
+    Rails.logger.info("!!!!!! AuthenticationsController#create !!!!!!")
+
+    super
+  end
+
+  def destroy
+    Rails.logger.info("!!!!!! AuthenticationsController#create !!!!!!")
+
+    super
+  end
+
+  def passthru
+    render status: 404, plain: "Not found. Authentication passthru in AuthenticationsController."
+  end
+
+  def failure
+    set_flash_message! :alert, :failure, kind: OmniAuth::Utils.camelize(failed_strategy.name), reason: failure_message
+    redirect_to after_omniauth_failure_path_for(resource_name)
+  end
+
+  protected
+
+  def failed_strategy
+    request.respond_to?(:get_header) ? request.get_header("omniauth.error.strategy") : request.env["omniauth.error.strategy"]
+  end
+
+  def failure_message
+    exception = request.respond_to?(:get_header) ? request.get_header("omniauth.error") : request.env["omniauth.error"]
+    error   = exception.error_reason if exception.respond_to?(:error_reason)
+    error ||= exception.error        if exception.respond_to?(:error)
+    error ||= (request.respond_to?(:get_header) ? request.get_header("omniauth.error.type") : request.env["omniauth.error.type"]).to_s
+    error.to_s.humanize if error
+  end
+
+  def after_omniauth_failure_path_for(scope)
+    new_session_path(scope)
+  end
+
+  def translation_scope
+    "devise.omniauth_callbacks"
+  end
+end
