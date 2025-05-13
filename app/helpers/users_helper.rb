c8ae2381ad3de43447265e061a8b865e946bf972
Chase Southard <chase.southard@gmail.com>
2025-05-13 15:53:47 -0400

    Updates User profile view with id profile info

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+  def pretty_print_provider_name(provider_name)
+    case provider_name
+    when "github"
+      "GitHub.com"
+    when "githubdr"
+      "GitHub Enterprise Cloud with Data Residency"
+    end
+  end
 end
