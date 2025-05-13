5a75874c95dae78c8fcd2cca08bc7a6cd3ecf0b6
Chase Southard <chase.southard@gmail.com>
2025-05-13 15:50:16 -0400

    Update routes with devise related configs

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { omniauth_callbacks: "omniauth" }
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users, only: :omniauth_callbacks, controllers: { omniauth_callbacks: "omniauth_callbacks" }, skip: [ :sessions, :registrations ]
 
   resources :users, only: [] do
     get "profile", on: :member
   end
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_scope :user do
+    # if you don't have the skip: [ :sessions, :registrations ] option above, then you need to use the following:
+    delete "/logout", to: "authentications#destroy"
+    delete "/users/sign_out", to: "devise/sessions#destroy", as: :destroy_user_session
+  end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
@@ -15,9 +20,10 @@ Rails.application.routes.draw do
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
-  # Defines the root path route ("/")
-  # root "posts#index"
   get "home", to: "home#index"
   get "home/index"
+
+  # Defines the root path route ("/")
+  # root "posts#index"
   root "home#index"
 end
